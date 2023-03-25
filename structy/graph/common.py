ea689505fd4bf11c7f705536237183e01db8ad00
baroncurtin2 <knowledge3@gmail.com>
2023-03-25 14:51:51 -0400

    python safe cracking

--- structy/graph/common.py
+++ structy/graph/common.py
@@ -12,14 +12,27 @@ EdgeList = list[tuple | list]
 class Edges:
     edge_list: EdgeList
 
-    def to_graph(self):
-        graph = defaultdict(list)
+    def to_graph(self) -> Graph:
+        graph = _new_graph()
 
-        for (node_a, node_b) in self.edge_list:
+        for node_a, node_b in self.edge_list:
             graph[node_a].append(node_b)
             graph[node_b].append(node_a)
 
         return graph
+
+    def to_dag(self) -> Graph:
+        graph = _new_graph()
+
+        for start, end in self.edge_list:
+            graph[start].append(end)
+            _ = graph[end]
+
+        return graph
+
+
+def _new_graph() -> Graph:
+    return defaultdict(list)
 
 
 @dataclass(slots=True)
@@ -29,7 +42,7 @@ class Prereqs:
     def to_graph(self):
         graph = defaultdict(list)
 
-        for (a, b) in self.pre_reqs_list:
+        for a, b in self.pre_reqs_list:
             graph[a].append(b)
 
         return graph
